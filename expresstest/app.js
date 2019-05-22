4d52f1da144b2a4ca768a3ccf57d7c46282d0dc6
Tianyi Shi <ShiTianyi2001@outlook.com>
2019-05-22 23:50:27 +0100

    0

--- expresstest/app.js
+++ expresstest/app.js
@@ -1,8 +1,53 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+	app = express(),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser");
+
+express.urlencoded({extended: true});
+
+mongoose.connect("mongodb://localhost/blog");
+
+var catSchema = new mongoose.Schema({
+	name: String,
+	age: Number
+});
+
+/* var Cat = mongoose.model("Cat", catSchema);
+
+var alan = new Cat({
+	name: "Alan",
+	age: 10
+});
+
+alan.save(function(err, cat) {
+	if (err) {
+		console.log("wrong");
+	} else {
+		console.log(cat);
+	}
+}); */
+
+Cat.create(
+	{
+		name: "Alan",
+		age: 10
+	},
+	function(err, cat) {
+		if (err) {
+			console.log("wrong");
+		} else {
+			console.log(cat);
+		}
+	}
+);
+
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-	res.render("home.ejs");
+	res.render("home");
 });
 
 app.listen(3000, function() {
