bece53e8ebf0df8d9e56fd7422554b582c4ab052
Tianyi Shi <ShiTianyi2001@outlook.com>
2019-05-26 20:48:44 +0100

    Update xinwen-lianbo.js

--- china-info/src/xinwen-lianbo.js
+++ china-info/src/xinwen-lianbo.js
@@ -5,14 +5,20 @@ $('.kuaixun h4:first-of-type').before('<h3 class="heading">其他</h3>');
 $('.zswz').click(function(){
     $('.zhongsheng, .kuaixun, .ruiping').removeClass('hide')
     $('.ruiping, .kuaixun').addClass('hide')
+    $('.zswz, .gjrp, .qb').removeClass('active')
+    $('.zswz').addClass('active')
 });
 
 $('.gjrp').click(function(){
     $('.zhongsheng, .kuaixun, .ruiping').removeClass('hide')
     $('.zhongsheng, .kuaixun').addClass('hide')
+    $('.zswz, .gjrp, .qb').removeClass('active')
+    $('.gjrp').addClass('active')
 });
 
 $('.qb').click(function(){
     $('.zhongsheng, .kuaixun, .ruiping').removeClass('hide')
+    $('.zswz, .gjrp, .qb').removeClass('active')
+    $('.qb').addClass('active')
 });
 
